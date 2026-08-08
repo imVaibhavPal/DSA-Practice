be24caded26ef129e13f5ccd1514a12d342785d7
imVaibhavPal <vkpal8300@gmail.com>
2026-08-08 16:56:00 +0530

    cpp practice

---
+++ String/Longest Palindrome substring/substring.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string longestPalindrome(string s) {
+
+    int start = 0;
+    int maxLength = 1;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        // Odd length palindrome
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < s.length() &&
+               s[left] == s[right]) {
+
+            if (right - left + 1 > maxLength) {
+                start = left;
+                maxLength = right - left + 1;
+            }
+
+            left--;
+            right++;
+        }
+
+        // Even length palindrome
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < s.length() &&
+               s[left] == s[right]) {
+
+            if (right - left + 1 > maxLength) {
+                start = left;
+                maxLength = right - left + 1;
+            }
+
+            left--;
+            right++;
+        }
+    }
+
+    return s.substr(start, maxLength);
+}
+
+int main() {
+
+    string s;
+
+    cout << "Enter a string: ";
+    cin >> s;
+
+    cout << "Longest Palindromic Substring: "
+         << longestPalindrome(s);
+
+    return 0;
+}
