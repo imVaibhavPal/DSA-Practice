0984824f4e70be06e5c163b562abe5aebae63563
imVaibhavPal <vkpal8300@gmail.com>
2026-08-11 23:55:56 +0530

    cpp practice

---
+++ Linked List/middle element of LL/middle.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node* findMiddle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main(){
+    Node* head = new Node(1);
+    head-> next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+
+    Node * middle= findMiddle(head);
+
+    cout<<"Middle element: "<<middle->data<<endl;
+
+    return 0;
+
+}
