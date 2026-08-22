662e130c60670b72ad012c1844beb1f2d1bbd9b9
imVaibhavPal <vkpal8300@gmail.com>
2026-08-22 20:31:55 +0530

    cpp practice

--- Stack/stack intro/intro.cpp
+++ Stack/stack intro/intro.cpp
@@ -2,9 +2,90 @@
 #include<stack>
 using namespace std;
 
+class Stack{
+    //properties
+    public:
+        int *arr;
+        int top;
+        int size;
+
+    //behavior
+    Stack(int size){
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if(size - top > 1){
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout<<"Stack Overflow"<<endl;
+        }
+
+    }
+
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"stack underflow "<<endl;
+        }
+
+    }
+
+    int peek(){
+        if(top >= 0)
+            return arr[top];
+
+        else{
+            cout<<"Stack is Empty"<<endl;
+            return -1;
+        }
+
+    }
+
+    bool isEmpty(){
+        if(top==-1)
+            return true;
+
+        else{
+            return false;
+        }
+    }
+};
+
 int main(){
 
-    //creating a stack
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(39);
+    st.push(43);
+
+    cout<< st.peek()<<endl;
+
+    st.pop();
+    cout<< st.peek() << endl;
+
+    st.pop();
+
+    cout<< st.peek() << endl;
+
+
+    if(st.isEmpty()){
+        cout<<"Stack is Empty mere dost "<<endl;
+    }
+    else{
+        cout<<"Stack is not Empty mere dost "<<endl;
+    }
+
+    /*creating a stack
     stack<int> s;
 
     //push operation
@@ -23,6 +104,9 @@ int main(){
     else{
         cout<<"stack is not empty "<<endl;
     }
+
+    cout<<"size of stack is "<<s.size()<<endl;
+    */
 
     return 0;
 }
