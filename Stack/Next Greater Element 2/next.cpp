81a5dae0e91cf977549af8a9f2f6891f9fd04855
imVaibhavPal <vkpal8300@gmail.com>
2026-09-04 23:10:15 +0530

    cpp practice

---
+++ Stack/Next Greater Element 2/next.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans(n, -1);
+    stack<int> s;
+
+    for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!s.empty() && nums[s.top()] <= nums[i % n]) {
+            s.pop();
+        }
+
+        if (i < n) {
+            ans[i] = s.empty() ? -1 : nums[s.top()];
+        }
+
+        s.push(i % n);
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 1};
+
+    vector<int> ans = nextGreaterElements(nums);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
